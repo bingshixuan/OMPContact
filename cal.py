72abc61855e2bee3b33071e63c3f87a0b75ba4a6
Fang Chao <bingshixuan@126.com>
2019-04-18 12:35:36 +0800

    Create cal.py

---
+++ cal.py
@@ -0,0 +1,41 @@
+import os
+import numpy as np
+from sklearn.metrics import matthews_corrcoef
+import sklearn.metrics as metrics
+from multiprocessing import Pool
+
+
+root='C:\\Users\\admin\\Desktop\\OMPcontact\\Omes\\testing\\'
+list = os.listdir(root)
+
+y_all = []
+y_all_pred = []
+
+for i in list:
+    if i.split('.',1)[0] == i:
+        continue
+    else:
+        y_real = []
+        y_pred = []
+        f = open(root+i)
+        lines = f.readlines()
+        f.close()
+        for j in lines:
+            y_real.append(float(j.split(' ',1)[0]))
+        
+        f = open(root+i.split('.',1)[0])
+        lines = f.readlines()
+        f.close()
+        for j in lines:
+            y_pred.append(float(j.split('\\',1)[0]))
+        
+        a, b, c, d = metrics.confusion_matrix(y_real, y_pred).ravel()
+        print([i, metrics.accuracy_score(y_real, y_pred), a, b, c, d, metrics.precision_score(y_real, y_pred), metrics.recall_score(y_real, y_pred), metrics.f1_score(y_real, y_pred), matthews_corrcoef(y_real, y_pred)])
+        y_all.append(y_real)
+        y_all_pred.append(y_pred)
+
+y_all=[y for x in y_all for y in x]
+y_all_pred=[y for x in y_all_pred for y in x]
+a, b, c, d = metrics.confusion_matrix(y_all, y_all_pred).ravel()
+print([metrics.accuracy_score(y_all, y_all_pred), a, b, c, d, metrics.precision_score(y_all, y_all_pred), metrics.recall_score(y_all, y_all_pred), metrics.f1_score(y_all, y_all_pred), matthews_corrcoef(y_all, y_all_pred)])
+
